c9cdc91be4bfd426e58d5ac4b207c29bda82aef3
Yosuke Mori <ykmori15@gmail.com>
2015-11-26 21:03:23 -0500

    updated application to parse batting data for 2014 and create a cluster model

--- src/main/scala/com/artificialclairvoyance/core/ArtificialClairvoyance.scala
+++ src/main/scala/com/artificialclairvoyance/core/ArtificialClairvoyance.scala
@@ -1,8 +1,12 @@
 package com.artificialclairvoyance.core
 
+import java.io.File
+
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
+import org.apache.spark.mllib.clustering.KMeans
+import org.apache.spark.mllib.linalg.Vectors
 
 /**
  * Top level application container.
@@ -11,13 +15,32 @@ import org.apache.spark.SparkConf
  **/
 object ArtificialClairvoyance {
   def main(args: Array[String]) {
-    // Should be some file on your system
-    val logFile = "/Users/yk_yosuke_mori/Documents/spark/artificial-clairvoyance/README.md"
+    // Set up spark context
     val conf = new SparkConf().setAppName("Artificial Clairvoyance")
     val sc = new SparkContext(conf)
-    val logData = sc.textFile(logFile, 2).cache()
-    val numAs = logData.filter(line => line.contains("a")).count()
-    val numBs = logData.filter(line => line.contains("b")).count()
-    println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+
+    // Should be some file on your system
+    // TODO: This should be abstracted to a data collector (specifies where the data comes from)
+    val battingFile = "src/test/resources/lahman-csv_2015-01-24/Batting.csv"
+    val rawBattingData = sc.textFile(battingFile, 2).cache()
+
+    // Parse the necessary data from this
+    // TODO: Need to abstract the parsing in a different object
+    val batters2014 = rawBattingData.map(_.split(",")).filter(line => line(1).equals("2014")&&line(5).toInt>=100)
+    val parsedData = batters2014.map {
+      line =>
+        Vectors.dense(line(12).toDouble, line(16).toDouble)
+    }
+
+    // Run it through K-MEANS
+    // TODO: Abstract the machine learning portion
+    val iterationCount = 1000
+    val clusterCount = 10
+    val clusterModel = KMeans.train(parsedData, clusterCount, iterationCount)
+    val clusterCenter = clusterModel.clusterCenters map (_.toArray)
+    println("Cost of this Model: %s".format(clusterModel.computeCost(parsedData)))
+    clusterCenter.foreach(center => println("Cluster Center: (RBI: %s, SO: %s)".format(center(0), center(1))))
+
+    // TODO: Visualize data
   }
 }
