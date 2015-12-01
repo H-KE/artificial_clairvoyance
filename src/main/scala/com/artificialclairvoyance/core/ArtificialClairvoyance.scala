decf59dc6c818f0cf5629e326de913f4ac69a6b1
Han Ke <kehan@Hans-MacBook-Pro.local>
2015-12-01 15:46:13 -0500

    some visual representation

--- src/main/scala/com/artificialclairvoyance/core/ArtificialClairvoyance.scala
+++ src/main/scala/com/artificialclairvoyance/core/ArtificialClairvoyance.scala
@@ -5,6 +5,8 @@ import org.apache.spark.SparkConf
 import org.apache.spark.mllib.clustering.KMeans
 import org.apache.spark.mllib.linalg.Vectors
 
+import java.io._
+
 /**
  * Top level application container.
  * This will set up the necessary context and invoke our application
@@ -24,8 +26,13 @@ object ArtificialClairvoyance {
     /* mlb */
     val battingFile = "src/test/resources/lahman-csv_2015-01-24/Batting.csv"
     val rawBattingData = sc.textFile(battingFile, 2).cache()
+<<<<<<< 5d11d6a5eb9938f1657a457a4685214c55afd77e
     val mlbPlayerFile = "src/test/resources/lahman-csv_2015-01-24/Master.csv"
     val rawMlbPlayerFile = sc.textFile(mlbPlayerFile, 2).cache()
+=======
+    val centersOutput = "src/test/resources/output/centers.csv"
+    val playersOutput = "src/test/resources/output/players.csv"
+>>>>>>> some visual representation
     /* nba */
     val basketballFile = "src/test/resources/nba/leagues_NBA_2015_per_game_per_game.csv"
     val rawNbaData = sc.textFile(basketballFile, 2).cache()
@@ -74,7 +81,7 @@ object ArtificialClairvoyance {
     // Cluster using K-means
     /* mlb */
     val iterationCountMLB = 1000
-    val clusterCountMLB = 10
+    val clusterCountMLB = 20
     // Produce the MLB clustering model
     val mlbClusterModel = KMeans.train(parsedBattingData, clusterCountMLB, iterationCountMLB)
     // Find centers of each cluster
@@ -99,7 +106,7 @@ object ArtificialClairvoyance {
     /* mlb */
     val mlbPlayersByGroup = batters2014.map{
       line => Array(
-        // Metaata
+        // Metadata
         Array(
           line(0),
           line(1)
@@ -114,6 +121,7 @@ object ArtificialClairvoyance {
       // Predict using the actual data
       player => mlbClusterModel.predict(Vectors.dense(player(1).map(_.toDouble)))
     }.collect()
+<<<<<<< 5d11d6a5eb9938f1657a457a4685214c55afd77e
     /* nba */
     val nbaPlayersByGroup = nba2014.map{
       line => Array(
@@ -138,6 +146,8 @@ object ArtificialClairvoyance {
       // Predict using the actual data
       player => nbaClusterModel.predict(Vectors.dense(player(1).map(_.toDouble)))
     }.collect()
+=======
+>>>>>>> some visual representation
 
     /**
      * Regression Algorithm.
@@ -154,44 +164,105 @@ object ArtificialClairvoyance {
     /* mlb */
     // Print total cost
     println("Cost of the MLB Model: %s".format(mlbClusterModel.computeCost(parsedBattingData)))
+    // Create a Document to represent the data
     // Print the average stat for each group
-    mlbClusterCenter.foreach(
-      center => println(
-        "Cluster Center: (Hit: %s, HR: %s)"
-          .format(center(0), center(1))
-      )
-    )
-    // Output each individual player in each group
-    for((group, players) <- mlbPlayersByGroup) {
-      players.foreach(
-        player => println(
-          "Group %s - Name: %s (Hit: %s, HR %s)"
-            .format(group, player(0)(0), player(1)(0), player(1)(1))
-        )
-      )
+
+    printToFile(new File(centersOutput)) {
+        p => {
+            p.println("hits,homeruns")
+            mlbClusterCenter.foreach(center => p.println("%s,%s".format(center(0), center(1))))
+        }
     }
-    /* nba */
-    // Print total cost
-    println("Cost of the NBA Model: %s".format(nbaClusterModel.computeCost(parsedNbaData)))
-    // Print the average stat for each group
-    nbaClusterCenter.foreach(
-      center => println(
-        "Cluster Center: (FG: %.2f, 3PM: %.2f, FT: %.2f, REB: %.2f, AST: %.2f, STL: %.2f, BLK: %.2f, TOV: %.2f, PTS: %.2f)"
-          .format(center(0), center(1), center(2), center(3), center(4), center(5), center(6), center(7), center(8))
-      )
-    )
-    // Output each individual player in each group
-    for((group, players) <- nbaPlayersByGroup) {
-      players.foreach(
-        player =>
-          println(
-          "Group %s - Name: %s (FG: %.2f, 3PM: %.2f, FT: %.2f, REB: %.2f, AST: %.2f, STL: %.2f, BLK: %.2f, TOV: %.2f, PTS: %.2f)"
-            .format(group, player(0)(0), player(1)(0).toDouble, player(1)(1).toDouble, player(1)(2).toDouble, player(1)(3).toDouble, player(1)(4).toDouble, player(1)(5).toDouble, player(1)(6).toDouble, player(1)(7).toDouble, player(1)(8).toDouble)
-        )
-      )
+
+    printToFile(new File(playersOutput)) {
+        p => {
+            p.println("cluster,player,hits,homeruns")
+            for((group, players) <- mlbPlayersByGroup) {
+                players.foreach(player => p.println("%s,%s,%s,%s".format(group, player(0)(0), player(1)(0), player(1)(1))))
+            }
+        }
     }
+    // Output each individual player in each group
+    // for((group, players) <- mlbPlayersByGroup) {
+    //     printToFile(new File(playersOutput)) {
+    //         p => players.foreach(player => p.println("%s,%s,%s,%s".format(group, player(0)(0), player(1)(0), player(1)(1))))
+    //     }
+    // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // /* nba */
+    // val iterationCountNBA = 10000
+    // val clusterCountNBA = 20
+    // // Produce the NBA clustering model
+    // val nbaClusterModel = KMeans.train(parsedNbaData, clusterCountNBA, iterationCountNBA)
+    // // Find centers of each cluster
+    // val nbaClusterCenter = nbaClusterModel.clusterCenters map (_.toArray)
+    // // Group the actual players into clusters
+    // val nbaPlayersByGroup = nba2014.map{
+    //   line => Array(
+    //     // Metadata
+    //     Array(
+    //       line(1)
+    //     ),
+    //     // Actual Data
+    //     Array(
+    //       line(10),
+    //       line(11),
+    //       line(20),
+    //       line(23),
+    //       line(24),
+    //       line(25),
+    //       line(26),
+    //       line(27),
+    //       line(29)
+    //     )
+    //   )
+    // }.groupBy{
+    //   player => nbaClusterModel.predict(Vectors.dense(player(1).map(_.toDouble)))
+    // }.collect()
+
+    // /* nba */
+    // // Print total cost
+    // println("Cost of the NBA Model: %s".format(nbaClusterModel.computeCost(parsedNbaData)))
+    // // Print the average stat for each group
+    // nbaClusterCenter.foreach(
+    //   center => println(
+    //     "Cluster Center: (FG: %.2f, 3PM: %.2f, FT: %.2f, REB: %.2f, AST: %.2f, STL: %.2f, BLK: %.2f, TOV: %.2f, PTS: %.2f)"
+    //       .format(center(0), center(1), center(2), center(3), center(4), center(5), center(6), center(7), center(8))
+    //   )
+    // )
+    // // Output each individual player in each group
+    // for((group, players) <- nbaPlayersByGroup) {
+    //   players.foreach(
+    //     player =>
+    //       println(
+    //       "Group %s - Name: %s (FG: %.2f, 3PM: %.2f, FT: %.2f, REB: %.2f, AST: %.2f, STL: %.2f, BLK: %.2f, TOV: %.2f, PTS: %.2f)"
+    //         .format(group, player(0)(0), player(1)(0).toDouble, player(1)(1).toDouble, player(1)(2).toDouble, player(1)(3).toDouble, player(1)(4).toDouble, player(1)(5).toDouble, player(1)(6).toDouble, player(1)(7).toDouble, player(1)(8).toDouble)
+    //     )
+    //   )
+    // }
 
     // Terminate the spark context
     sc.stop()
   }
+
+  def printToFile(f: java.io.File)(op: java.io.PrintWriter => Unit) {
+    val p = new java.io.PrintWriter(f)
+    try { op(p) } finally { p.close() }
+  }
 }
+
+
