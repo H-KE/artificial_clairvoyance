fa96179d3c5b238e471c5eb04a33f5c9166cc720
Yosuke Mori <ykmori15@gmail.com>
2016-03-11 14:58:28 -0500

    fixed run script to remove junk files

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+rm -r ${DIR}/../app/resources/output/nba_players_historical2
+rm -r ${DIR}/../app/resources/output/mlb_players_historical2
 ${SPARK_HOME}/bin/spark-submit --packages com.databricks:spark-csv_2.10:1.2.0 --class "com.artificialclairvoyance.core.ArtificialClairvoyance" --master local[4] ${DIR}/../target/scala-2.10/artificial-clairvoyance_2.10-0.0.1.jar
