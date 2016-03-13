bc0a4aa2c6a337dd37c238dc3b96315be7423831
Yosuke Mori <ykmori15@gmail.com>
2016-03-13 15:54:57 -0400

    changed save location of collector script

--- collector/python/nba_scraper.py
+++ collector/python/nba_scraper.py
@@ -12,7 +12,7 @@ seasonFrom = 1980
 seasonTo = 2015
 
 header = ["PlayerId", "Name", "Season", "Age", "Position", "Team", "Games", "MP", "FG%", "3PM", "3P%", "FT%", "REB", "AST", "STL", "BLK", "TOV", "PTS"]
-with open('/tmp/artificialclairvoyance/nba/nbaPlayerTotals.csv', 'w') as out:
+with open('/tmp/nbaPlayerTotals.csv', 'w') as out:
 	writer = csv.writer(out, delimiter=',')
 	writer.writerow(header)
 	year = seasonFrom
