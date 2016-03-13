ad24bf6a9669ae198bfbd2b8e6f6ba514fad52be
Yosuke Mori <ykmori15@gmail.com>
2016-03-13 18:51:02 -0400

    changed output file name

--- src/test/python/aggregate/aggregate_parts.py
+++ src/test/python/aggregate/aggregate_parts.py
@@ -2,7 +2,7 @@ import glob, os
 
 
 
-#run in shell script after Artifical Clairvoyance runs 
+#run in shell script after Artifical Clairvoyance runs
 #Change path if want to run standalone
 path = "app/resources/output/"
 
@@ -11,29 +11,29 @@ folder_list = ["mlb_players_current2",
 			   "nba_players_current2",
 			   "nba_players_historical2"]
 
-output_files  = ["mlb_players_current.csv", 
-				 "mlb_players_historical.csv", 
-				 "nba_players_current.csv", 
+output_files  = ["mlb_players_match.csv",
+				 "mlb_players_historical.csv",
+				 "nba_players_match.csv",
 				 "nba_players_historical.csv"]
 
 
 for folder, output_file in zip(folder_list, output_files):
-	
+
 	out = open(os.path.join(path,output_file), "w" )
 	first_file = 1
-	
+
 	for file in os.listdir(path + folder):
-		
+
 		if file.startswith("part"):
 			f = open(path+folder+"/" + file, "r")
 			if first_file:
 				first_file = 0
 			else:
 				next(f)
-			
+
 			for line in f:
 				out.write(line)
-			 
+
 			f.close()
 
 	out.close()
