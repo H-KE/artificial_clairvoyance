08211eefe84449b0df0050560cf5b29827e255f4
aehank <aehank@gmail.com>
2016-03-15 18:45:33 -0400

    updated mlb accuracy test script

--- src/test/python/accuracy/mlb_accuracy_from_file.py
+++ src/test/python/accuracy/mlb_accuracy_from_file.py
@@ -4,8 +4,10 @@ import csv
 import matplotlib
 
 season = 2015
-column = "HR"
-file = "mlb_predictions.csv"
+column = "HR" #target variable
+file = "../../../../app/resources/output/mlb_predictions.csv"
+
+out = open('../../../../app/resources/output/mlb_players_current'+column+'.csv', 'w')
 
 # We will compare results with data pulled directly from online
 br = mechanize.Browser()
@@ -17,9 +19,15 @@ header = table.find('thead')
 header_names = header.find('tr').find_all('th')
 header_index_lookup = {}
 i = 0
+
+h_names = []
+
 for header_name in header_names:
+    h_names.append(header_name.text.encode('utf-8'))
     header_index_lookup[header_name.text] = i
     i = i+1
+
+out.write('PlayerId'+','+','.join(str(x) for x in h_names[1:]) + '\n')
 # Create a dictionary for finding the index of the player of interest
 rows = table.find_all('tr', attrs={'class':'full_table'})
 player_index_lookup = {}
@@ -28,8 +36,10 @@ for row in rows:
     player_index_lookup[row.find_all('td')[1].a.get('href').split('/')[-1].split('.')[0]] = i
     i = i+1
 errnum = 0
+noexist = 0
 # Dealing with the actual dataset we want to verify
 with open(file, 'r') as dataset:
+
     reader = csv.reader(dataset)
     row_num = 0
     dataset_header_index_lookup = {}
@@ -42,23 +52,32 @@ with open(file, 'r') as dataset:
         else:
             try:
                 player_id = row_dataset[dataset_header_index_lookup['PlayerId']]
-                age = row_dataset[dataset_header_index_lookup['Age']]
+                #age = row_dataset[dataset_header_index_lookup['Age']]
                 # Get all the player's data using the player_id as the lookup key
                 player = rows[player_index_lookup[player_id]].find_all('td')
-                actual_per_game = float(player[header_index_lookup[column]].text)#/int(player[header_index_lookup['G']].text)
+                stats = []
+
+                for p in player:
+                    stats.append(p.text.encode('utf-8'))
+
+                actual_per_game = float(player[header_index_lookup[column]].text)
                 predicted_per_game = float(row_dataset[dataset_header_index_lookup[column]])
-                #exit()
+
                 if actual_per_game > 0.0:
                     error = abs(predicted_per_game - actual_per_game)/actual_per_game
                 else:
                     error = 1
                 if error > 0.1:
                     errnum = errnum+1
-                    print "{} {} {}: Actual: {} Expected: {}, Error: {}".format(player[header_index_lookup['Name']].text.encode('utf-8'), player_id, age, actual_per_game, predicted_per_game, error)
+                    print "{} {}: Actual: {} Expected: {}, Error: {}".format(player[header_index_lookup['Name']].text.encode('utf-8'), player_id, actual_per_game, predicted_per_game, error)
+                out.write(player_id + ',' + ','.join(str(x) for x in stats[1:]) + '\n')
             except KeyError:
                 # For players that are not in the league anymore
                 print "{} does not exist".format(row_dataset[dataset_header_index_lookup['PlayerId']])
+                noexist = noexist + 1
         row_num = row_num+1
 
-print "{}/{} Error Rate".format(errnum, row_num)
+out.close()
+
+print "{}/{} Error Rate".format(errnum, row_num-noexist-1)
 
